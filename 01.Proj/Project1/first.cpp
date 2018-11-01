71e0bc62309e7dbd23c6f119bee7ae14712979a6
VladStashkevych <ladserp@gmail.com>
2018-11-01 16:28:08 +0200

    add 01.Proj

---
+++ 01.Proj/Project1/first.cpp
@@ -0,0 +1,34 @@
+#include "iostream"
+#include "cmath"
+
+using namespace std;
+
+int main() {
+	//тіло ф-ії
+
+	cout << "    " << "    " << "    * \n";
+	cout << "    " << "    " << "   * *\n";
+	cout << "    " << "    " << "  * * *\n";
+	cout << "    " << "    " << " * * * *\n";
+	cout << "    " << "    " << "* * * * *\n";
+	cout << "    " << "* * * * * * * * *\n";
+	cout << "    " << "*               *\n";
+	cout << "    " << "*               *\n";
+	cout << "    " << "*               *\n";
+	cout << "    " << "* * * * * * * * *\n";
+	cout << "    " << "* * * * " << "  * * * * " << endl;
+	cout << "    " << "* * * * " << "  * * * * " << endl;
+	cout << "    " << "* * * * " << "  * * * * " << endl;
+	cout << "    " << "* * * * " << "  * * * * " << endl;
+	cout << endl;
+	cout << "* * *  * * *  * * *  *     *    * * * \n";
+	cout << "*   *  *   *  *      *     *    *   * \n";
+	cout << "* * *  *   *  *      *   * *    * * * \n";
+	cout << "*      *   *  *   *  * *   *      * * \n";
+	cout << "*      * * *  * * *  *     *    *   * \n";
+
+
+	system("pause");
+
+	return 0;
+}
